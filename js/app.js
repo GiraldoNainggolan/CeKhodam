ffbd013d434783c5c3fb8a3336a28516a6b74daf
GiraldoNainggolan <220441100064@student.trunojoyo.ac.id>
2024-12-12 23:33:37 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,235 @@
+// const khodamNames = [
+//   "Kuntilanak (Jawa/Melayu)",
+//   "Genderuwo (Jawa)",
+//   "Wewe Gombel (Jawa Tengah)",
+//   "Pocong (Nasional)",
+//   "Sundel Bolong (Jawa)",
+//   "Palasik (Minangkabau, Sumatra Barat)",
+//   "Leak (Bali)",
+//   "Begu Ganjang (Batak, Sumatra Utara)",
+//   "Cindaku (Kerinci, Jambi)",
+//   "Orang Bunian (Sumatra dan Kalimantan)",
+//   "Banaspati (Jawa/Bali)",
+//   "Hantu Bungkus (Dayak, Kalimantan)",
+//   "Kolor Ijo (Banten/Jawa Barat)",
+//   "Hantu Jerangkong (Sunda)",
+//   "Nyi Roro Kidul (Jawa)",
+//   "Lampor (Jawa Tengah)",
+//   "Jenglot (Jawa/Sumatra)",
+//   "Hantu Laut (Kepulauan Riau)",
+//   "Hantu Langkisau (Riau)",
+//   "Tuyul (Jawa)",
+//   "Sibarak (Minangkabau, Sumatra Barat)",
+//   "Antu Banyu (Sumatra Selatan)",
+//   "Makhluk Niskala (Bali/Sunda)",
+//   "Poppo (Sulawesi Selatan)",
+//   "Pamali (Sulawesi Selatan)",
+//   "Si Uteun (Aceh)",
+//   "Hantu Kuburan Lama (Sumatra Selatan)",
+//   "Siluman Harimau (Batak, Sumatra Utara)",
+//   "Siluman Sungai (Jambi)",
+//   "Sukma Berinduk (Lampung)",
+//   "Yuyurungku (Sunda)",
+//   "Hantu Pasir (Kalimantan Timur)",
+//   "Hantu Babakan (Banten)",
+//   "Arwah Perang Dayak (Kalimantan)",
+//   "Penunggu Lombok (Nusa Tenggara Barat)",
+//   "Teke Teke (Nusa Tenggara Timur)",
+//   "Sujanggi (Papua)",
+//   "Kokoruga (Papua)",
+//   "Hantu Barong (Bali)",
+//   "Ronggeng (Jawa)",
+//   "Fiery Banaspati (Jawa/Bali)",
+//   "Hantu Kemang (Bengkulu)",
+//   "Makhluk Pulau Penyengat (Kepulauan Riau)",
+//   "Eyang Penunggu (Jawa)",
+//   "Hantu Air Way Kanan (Lampung)",
+//   "Induak Banyu (Minangkabau, Sumatra Barat)",
+//   "Arwah Nyaring (Kalimantan Selatan)",
+//   "Pohon Pisang Keramat (Nasional)",
+//   "Singa Introvert (Fiksi Nasional)",
+//   "Hantu Lantong (Bugis, Sulawesi Selatan)",
+//   "Ipo-ipo (Makassar, Sulawesi Selatan)",
+//   "Anjat Kemuning (Dayak, Kalimantan Tengah)",
+//   "Karuhun (Sunda, Jawa Barat)",
+//   "Danyang (Jawa Tengah)",
+//   "Orang Tinggi (Mentawai, Sumatra Barat)",
+//   "Lembuswana (Kutai, Kalimantan Timur)",
+//   "Makhluk Tidong (Tidong, Kalimantan Utara)",
+//   "Sambau (Suku Anak Dalam, Jambi)",
+//   "Buta Ijo (Jawa)",
+//   "Silu Bolong (Bajo, Nusa Tenggara)",
+//   "Rambut Geni (Bali)",
+//   "Opo Patu’ (Minahasa, Sulawesi Utara)",
+//   "Bulu Lawang (Toraja, Sulawesi Selatan)",
+//   "Lelebati (Ambon, Maluku)",
+//   "Hantu Marabahan (Banjar, Kalimantan Selatan)",
+//   "Makhluk Muara Pawan (Ketapang, Kalimantan Barat)",
+//   "Hantu Bukit Barisan (Sumatra Utara)",
+//   "Siluman Ular (Jawa dan Kalimantan)",
+//   "Orang Dondo (Sulawesi Tengah)",
+//   "Buaya Putih (Nasional, Penunggu Sungai)",
+//   "Nyai Blorong (Jawa)",
+//   "Sigale-gale (Batak, Sumatra Utara)",
+//   "Ratu Pantai Barat (Banten)",
+//   "Hantu Tolikara (Papua)",
+//   "Sijimung (Aceh)",
+//   "Kuyang (Kalimantan Timur)",
+//   "Titi Gantung (Dayak Iban, Kalimantan Barat)",
+//   "Hantu Kopeah (Gorontalo)",
+//   "Susu Babi (Sumatra)",
+//   "Siluman Jembatan Ampera (Sumatra Selatan)",
+//   "Hantu Huma (Sunda)",
+//   "Makhluk Pagaralam (Sumatra Selatan)",
+// ];
+
+const khodamNames = [
+  "Singa Afrika (Afrika)", 
+  "Harimau Bengal (India)", 
+  "Macan Tutul Salju (Himalaya)", 
+  "Beruang Grizzly (Amerika Utara)", 
+  "Serigala Arktik (Arktik)", 
+  "Buaya Nil (Afrika)", 
+  "Komodo (Indonesia)", 
+  "Jaguar (Amazon, Amerika Selatan)", 
+  "Puma (Amerika Utara dan Selatan)", 
+  "Ular Anaconda (Amazon, Amerika Selatan)", 
+  "Hiu Putih Besar (Lautan Dunia)", 
+  "Piranha (Sungai Amazon)", 
+  "Elang Emas (Amerika Utara dan Eurasia)", 
+  "Gajah Afrika (Afrika)", 
+  "Rusa Kutub (Arktik)", 
+  "Kobra Raja (Asia Selatan)", 
+  "Buaya Muara (Asia Tenggara, Australia)", 
+  "Cheetah (Afrika)", 
+  "Hyenah Bintik (Afrika)", 
+  "Serigala Abu-abu (Amerika Utara, Eurasia)", 
+  "Babi Hutan (Asia, Afrika, Eropa)", 
+  "Orca (Samudra Arktik dan Antarktika)", 
+  "Beruang Kutub (Arktik)", 
+  "Raja Udang (Sungai dan Danau Dunia)", 
+  "Elang Harpy (Amazon, Amerika Selatan)", 
+  "Badak Putih (Afrika)", 
+  "Kudanil (Afrika)", 
+  "Ikan Barracuda (Lautan Dunia)", 
+  "Lumba-Lumba Spinner (Samudra Hindia dan Pasifik)", 
+  "Harimau Siberia (Siberia, Rusia)", 
+  "Paus Biru (Samudra Dunia)", 
+  "Hiu Macan (Laut Tropis)", 
+  "Singa Laut (Samudra Pasifik)", 
+  "Macan Dahan (Asia Tenggara)", 
+  "Gorila Gunung (Afrika)", 
+  "Panda Raksasa (Tiongkok)", 
+  "Elang Botak (Amerika Utara)", 
+  "Lynx (Eropa dan Asia)", 
+  "Ikan Pari Raksasa (Samudra Hindia dan Pasifik)", 
+  "Burung Kasuari (Australia dan Papua)", 
+  "Buaya Gharial (India)", 
+  "Ular Mamba Hitam (Afrika)", 
+  "Kanguru Merah (Australia)", 
+  "Dhole (Asia Selatan dan Timur)", 
+  "Serigala Ethiopia (Ethiopia)", 
+  "Macan Emas Afrika (Afrika)", 
+  "Rubah Arktik (Arktik)", 
+  "Ikan Pedang (Samudra Dunia)", 
+  "Elang Filipina (Filipina)", 
+  "Harimau Malaya (Malaysia)", 
+  "Beruang Hitam Asia (Asia)", 
+  "Rakun (Amerika Utara)", 
+  "Kucing Hutan Karakal (Afrika dan Asia)", 
+  "Burung Hantu Salju (Arktik)", 
+  "Buaya Amerika (Amerika Tengah dan Selatan)", 
+  "Bison Amerika (Amerika Utara)", 
+  "Capung Raksasa (Hutan Tropis Dunia)", 
+  "Gajah India (India)", 
+  "Macan Tutul Hitam (Afrika dan Asia)", 
+  "Burung Rangkong (Asia Tenggara)", 
+  "Simpanse (Afrika)", 
+  "Tarsius (Sulawesi, Indonesia)", 
+  "Berang-berang Raksasa (Amazon, Amerika Selatan)", 
+  "Platipus (Australia)", 
+  "Burung Kakapo (Selandia Baru)", 
+  "Burung Kiwi (Selandia Baru)", 
+  "Antelop Sable (Afrika)", 
+  "Ikan Pari Mobula (Samudra Hindia dan Pasifik)"
+];
+
+
+function cekKhodam() {
+  // Menampilkan loading screen
+  const loadingScreen = document.getElementById("loading");
+  loadingScreen.style.visibility = "visible";
+  loadingScreen.style.opacity = "1";
+  loadingScreen.style.transition = "visibility 0s, opacity 0.3s ease-in-out"; // pastikan transisi halus
+
+  // Mengatur waktu tunggu untuk simulasi pemrosesan
+  setTimeout(() => {
+    // Menyembunyikan loading screen setelah 3 detik
+    loadingScreen.style.visibility = "hidden";
+    loadingScreen.style.opacity = "0";
+
+    // Memilih nama khodam secara acak
+    const randomIndex = Math.floor(Math.random() * khodamNames.length);
+    const randomKhodam = khodamNames[randomIndex];
+
+    const nama = document.getElementById("nama").value.trim();
+
+    if (nama === "") {
+      alert("Nama tidak boleh kosong!");
+      return;
+    }
+
+    // Kasus khusus untuk "mbak niyak"
+    if (
+      nama == "Niyak" ||
+      nama == "Utees" ||
+      nama == "Niya" ||
+      nama == "fudhla" ||
+      nama == "Mahbatul" ||
+      nama == "Utsniya Fudhla" ||
+      nama == "Utsniya Mahbatul Fudhla" ||
+      nama == "Utsniya"
+    ) {
+      const resultContainer = document.getElementById("result");
+      resultContainer.innerHTML = `<p class="text-2xl text-white-500 font-bold">"${nama}"</p><br/>
+    <p class="text-2xl text-white-500 font-bold">Kodam : Tuyul Ceperet</p>`;
+      const inputNama = document.getElementById("nama");
+      inputNama.value = "";
+      return;
+    }
+
+    // Kasus khusus untuk "mbak yayas"
+    if (
+      nama == "Akyas" ||
+      nama == "Yayas" ||
+      nama == "Aya" ||
+      nama == "Aya'" ||
+      nama == "Ayak" ||
+      nama == "Akyas mauqiyyah" ||
+      nama == "Mauqiyyah"
+    ) {
+      const resultContainer = document.getElementById("result");
+      resultContainer.innerHTML = `<p class="text-2xl text-white-500 font-bold">"${nama}"</p><br/>
+    <p class="text-2xl text-white-500 font-bold">Kodam : Kentut Berdahak</p>`;
+      const inputNama = document.getElementById("nama");
+      inputNama.value = "";
+      return;
+    }
+
+    // Menampilkan khodam acak jika tidak masuk dalam kategori khusus
+    console.log(`Khodam yang muncul: ${randomKhodam}`);
+    const resultContainer = document.getElementById("result");
+    resultContainer.innerHTML = `<p class="text-2xl text-white-500 font-bold">"${nama}"</p><br/>
+    <p class="text-2xl text-white-500 font-bold">Kodam : ${randomKhodam}</p>`;
+
+    const inputNama = document.getElementById("nama");
+    inputNama.value = "";
+  }, 3000); // Setelah 3 detik, tampilkan hasil
+}
+
+// Menangani pengiriman form
+const form = document.getElementById("khodamForm");
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Mencegah form dari reload halaman
+  cekKhodam();
+});
